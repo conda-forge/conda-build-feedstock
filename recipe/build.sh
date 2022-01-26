4d63e410b67ed91e92e200191df93035c6d9afb7
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-01-26 09:15:57 +0100

    Install bdist_conda.py also to setuptools inlcuded distutils folder

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,10 @@ ${PYTHON} setup.py install --single-version-externally-managed --record=record.t
 
 cp bdist_conda.py "${PREFIX}/lib/python${PY_VER}/distutils/command/"
 
+# required for distutils included in setuptools >=60.0.0
+# see also https://setuptools.pypa.io/en/latest/history.html#v60-0-0
+SETUPTOOLS_DISTUTILS="${PREFIX}/lib/python${PY_VER}/site-packages/setuptools/_distutils/command/"
+mkdir -p "${SETUPTOOLS_DISTUTILS}"
+cp bdist_conda.py "${SETUPTOOLS_DISTUTILS}"
+
 rm -f "${PREFIX}/bin/conda"
